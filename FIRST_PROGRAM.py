2539de5035fa345afc02edead5cad3532550a4bd
plk29a <plk29a@gmail.com>
2020-02-02 01:46:56 +0100

    funkcja input()

---
+++ FIRST_PROGRAM.py
@@ -0,0 +1,12 @@
+#funkcja input(), str(), int()
+
+print('Hi, \nwhat is your name?') #Pyta o imie.
+Name = input()
+
+print(Name + ', how old are you') #pyta o wiek
+Wiek = input()
+
+print('You born in ' + str(2020 - int(Wiek))) #podaje datę urodzenia
+
+#Sprawdzenie czy wiek jest liczbą z przedziału 0-99
+
