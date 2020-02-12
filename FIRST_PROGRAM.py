26fc8010deaa3f4bf6c150c924598966ab5d993f
plk29a <plk29a@gmail.com>
2020-02-12 23:50:49 +0100

    Wymuszenie podania imienia

--- FIRST_PROGRAM.py
+++ FIRST_PROGRAM.py
@@ -3,10 +3,19 @@
 print('Hi, \nwhat is your name?') #Pyta o imie.
 Name = input()
 
+
+Name = Name.title()
+if Name: #pusty string ma wartość falsey.
+    print('Nice to meet you, ', Name)
+else:
+    print('Noname is no name :(')
+
 print(Name + ', how old are you') #pyta o wiek
 Wiek = input()
 
+#Sprawdzenie czy wiek jest liczbą z przedziału 0-99
+
 print('You born in ' + str(2020 - int(Wiek))) #podaje datę urodzenia
 
-#Sprawdzenie czy wiek jest liczbą z przedziału 0-99
+
 
