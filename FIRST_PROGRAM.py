8f5c2c3877cf90a869c3e12bec051fabb4a7f5e7
plk29a <plk29a@gmail.com>
2020-02-13 22:43:25 +0100

    dodanie paru dodatków, jak sprawdzanie wieku

--- FIRST_PROGRAM.py
+++ FIRST_PROGRAM.py
@@ -1,6 +1,6 @@
-#funkcja input(), str(), int()
+ #funkcja input(), str(), int()
 
-print('Hi, \nwhat is your name?') #Pyta o imie.
+print('Hi, \nwhat is your nickname?') #Pyta o imie.
 Name = input()
 
 
@@ -10,9 +10,20 @@ if Name: #pusty string ma wartość falsey.
 else:
     print('Noname is no name :(')
 
-print(Name + ', how old are you') #pyta o wiek
-Wiek = input()
 
+
+while True:
+    print(Name + ', how old are you?') #pyta o wiek
+    Wiek = input() #podajesz wiek
+
+    try:
+        int(Wiek)
+        if int(Wiek) <= 99 and int(Wiek) > 0: #sprawdza czy jest to liczba od 0 do 99 i przerywa pętle
+            break
+        else:
+            print('Are you sure?') #podaje komunikat jeśli jest poza przedziałem
+    except ValueError:    
+        print('it is not a number.') 
 #Sprawdzenie czy wiek jest liczbą z przedziału 0-99
 
 print('You born in ' + str(2020 - int(Wiek))) #podaje datę urodzenia
