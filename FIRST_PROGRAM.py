f0f900f6a16d085e52cd096b2a1477812f881dfd
plk29a <plk29a@gmail.com>
2020-02-16 15:55:55 +0100

    adjust to pep8

--- FIRST_PROGRAM.py
+++ FIRST_PROGRAM.py
@@ -1,23 +1,24 @@
 # funkcja input(), str(), int()
 
-name = input('Hi, \nwhat is your nickname?')
+name = input('Hi, \nwhat is your nickname?\n')
 name = name.title()
 
-if name: #value of empty string is False
+if name:  # value of empty string is False
     print('Nice to meet you,', name)
 else:
-    print('Noname is no name :(')
+    print('Noname is no name:(')
 
 while True:
-    age = input('{}, how old are you?'.format(name)) 
+    age = input('{}, how old are you?\n'.format(name))
 
     try:
         int(age)
-        if int(age) > 0 and int(age) <= 99:
+        if 0 < int(age) <= 99:
             break
         else:
-            print('Are you sure?')
+            print('\nAre you sure?')
     except ValueError:    
-        print('It is not a number.') 
+        print('It is not a number.')
 
 print('You are born in ' + str(2020 - int(age)))
+input('<press enter to exit>')
